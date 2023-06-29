9f8fbf273bb4ea18464923a148faff17a1e5bcda
Jakira Dunson <jakiradunson@gmail.com>
2023-06-29 09:30:36 -0500

    files being pushed

---
+++ src/test/java/jakira/springblog/CodeupSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package jakira.springblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
