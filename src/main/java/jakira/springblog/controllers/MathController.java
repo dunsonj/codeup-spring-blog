f81e8067483ce66781508a963989a533a46920a7
Jakira Dunson <jakiradunson@gmail.com>
2023-06-29 16:27:57 -0500

    exerise part two

--- src/main/java/jakira/springblog/controllers/MathController.java
+++ src/main/java/jakira/springblog/controllers/MathController.java
@@ -2,9 +2,39 @@ package jakira.springblog.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(path = "/math")
 public class MathController {
+    @GetMapping("/add/{a}/and/{b}")
+    @ResponseBody
+    public int add (@PathVariable int a, @PathVariable int b){
+        return a + b;
+    }
+    @GetMapping("/subtract/{a}/from/{b}")
+    @ResponseBody
+    public int subtract (@PathVariable int a, @PathVariable int b){
+        return b - a;
+    }
+
+    @GetMapping("/multiply/{a}/and/{b}")
+    @ResponseBody
+    public int multiply (@PathVariable int a, @PathVariable int b){
+        return a * b;
+    }
+
+    @GetMapping("/divide/{a}/by/{b}")
+    @ResponseBody
+    public int divide (@PathVariable int a, @PathVariable int b){
+        return a / b ;
+    }
+
+
+
+
+
+
 }
