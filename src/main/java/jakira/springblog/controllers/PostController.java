51e86b8e2472fc3c7bf2806fe3fd65602c354a35
Jakira Dunson <jakiradunson@gmail.com>
2023-06-29 16:51:04 -0500

    exerise part three

--- src/main/java/jakira/springblog/controllers/PostController.java
+++ src/main/java/jakira/springblog/controllers/PostController.java
@@ -2,9 +2,31 @@ package jakira.springblog.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path = "/controller")
+@RequestMapping(path = "/posts")
 public class PostController {
+    @GetMapping()
+    @ResponseBody
+    public String index(){
+        return "post index page";
+    }
+    @GetMapping("/{id}")
+    @ResponseBody
+    public String detail(@PathVariable Long id){
+        return "view an indiviual post " + id;
+    }
+    @GetMapping("/create")
+    @ResponseBody
+    public String insert(){
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    public String saveNewPost(){
+        return "create a new post";
+    }
+
 }
