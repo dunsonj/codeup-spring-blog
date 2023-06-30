2b1a1b0ca8caa1ddc6c3278284a61163f7949a43
Jakira Dunson <jakiradunson@gmail.com>
2023-06-30 09:09:46 -0500

    lesson

--- src/main/java/jakira/springblog/controllers/ViewsLec.java
+++ src/main/java/jakira/springblog/controllers/ViewsLec.java
@@ -1,4 +1,27 @@
 package jakira.springblog.controllers;
 
 public class ViewsLec {
+    /*
+    Thymleaf:
+    Known s natural templates
+    Uses th: prefex in the HTML File
+
+
+
+
+    GET/POST Request:
+
+
+    Thymeleaf Utilites:
+
+
+
+    Partials:
+
+
+     */
+
+
+
+
 }
