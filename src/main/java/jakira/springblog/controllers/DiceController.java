ad77479c1e7ce5c7645cca421a6d1b025bd80bfc
Jakira Dunson <jakiradunson@gmail.com>
2023-06-30 16:13:10 -0500

    exercise

--- src/main/java/jakira/springblog/controllers/DiceController.java
+++ src/main/java/jakira/springblog/controllers/DiceController.java
@@ -2,7 +2,11 @@ package jakira.springblog.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
 public class DiceController {
@@ -10,4 +14,19 @@ public class DiceController {
     public String dice(){
         return "rolldice";
     }
+
+    @GetMapping("/roll-dice/${num}")
+    public  String diceRoll(@PathVariable int num, Model model){
+        int rand_int = ThreadLocalRandom.current().nextInt(1,7);
+        model.addAttribute("result", rand_int);
+        model.addAttribute("choice", num);
+
+        if (num == rand_int){
+            model.addAttribute("match", "Your number matched!");
+        } else {
+            model.addAttribute("match", "Your did not number matched!");
+        }
+        return "rolldice";
+    }
+
 }
