58726dd94a73cd9b2da980e8c7d0753db0d3b1b3
Jakira Dunson <jakiradunson@gmail.com>
2023-07-05 09:45:55 -0500

    lec

--- src/main/java/jakira/springblog/controllers/DiceController.java
+++ src/main/java/jakira/springblog/controllers/DiceController.java
@@ -1,11 +1,12 @@
 package jakira.springblog.controllers;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Controller
@@ -15,18 +16,36 @@ public class DiceController {
         return "rolldice";
     }
 
-    @GetMapping("/roll-dice/${num}")
-    public  String diceRoll(@PathVariable int num, Model model){
+    @GetMapping("/roll-dice/{num}")
+    public String diceRoll(@PathVariable int num, Model model){
         int rand_int = ThreadLocalRandom.current().nextInt(1,7);
-        model.addAttribute("result", rand_int);
-        model.addAttribute("choice", num);
+        int rand_int2 = ThreadLocalRandom.current().nextInt(1,7);
+        int rand_int3 = ThreadLocalRandom.current().nextInt(1,7);
+        int rand_int4 = ThreadLocalRandom.current().nextInt(1,7);
+        int rand_int5 = ThreadLocalRandom.current().nextInt(1,7);
 
-        if (num == rand_int){
-            model.addAttribute("match", "Your number matched!");
-        } else {
-            model.addAttribute("match", "Your did not number matched!");
+        List<Integer> dicerolls = new ArrayList<>();
+
+        dicerolls.add(rand_int);
+        dicerolls.add(rand_int2);
+        dicerolls.add(rand_int3);
+        dicerolls.add(rand_int4);
+        dicerolls.add(rand_int5);
+
+        model.addAttribute("result",dicerolls);
+        model.addAttribute("choice",num);
+
+        int bucket = 0;
+
+        for (var i = 0; i< dicerolls.size(); i++){
+            if (num == dicerolls.get(i)){
+                bucket++;
+            }
         }
+
+        model.addAttribute("match",bucket);
+
+
         return "rolldice";
     }
-
 }
