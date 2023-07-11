edf046ebcdc848096b475ec4b07618e56ee43d98
Jakira Dunson <jakiradunson@gmail.com>
2023-07-11 12:01:49 -0500

    exercise

--- src/main/java/jakira/springblog/models/User.java
+++ src/main/java/jakira/springblog/models/User.java
@@ -33,4 +33,13 @@ public class User {
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "creator")
     private List<Post> posts;
 
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
+
+
 }
